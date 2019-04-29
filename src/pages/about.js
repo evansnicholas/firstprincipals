a1a0c1264bb2b3dde71aee978a49dd7383858611
Nicholas Evans <nick@nicholass-mbp.home>
2019-04-29 22:04:32 +0200

    Improving about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,31 @@
 import React from "react";
 import Site from "../components/Site";
-import styles from "./about.modules.scss";
+import aboutImage from "../components/images/about.jpeg";
 
 export default () => (
   <Site>
-      <p className="pv4 ph0-ns ph2">First Principals are Nick and Jeroen. More about them soon.</p>
+    <article className="db pt4 pb2 ph3 ph2-l mb4 bt bb b--black-10 br2-ns no-underline bg-white black">
+      <div className="flex flex-column flex-row-ns">
+        <div className="pr3-ns mb4 mb0-ns w-100 w-40-ns">
+          <img
+            src={aboutImage}
+            className="br-100 db"
+            alt="Nick & Jeroen."
+            />
+        </div>
+        <div className="w-100 w-60-ns pl3-ns">
+          <p className="f5 lh-copy tj">
+            <span className="b">First Principals</span> is <span className="b">Jeroen</span> and <span className="b">Nick's </span>
+             (sustainable) clothing project, based in Utrecht. It all started in 2017 with a screen-printing course at 
+            <a className="link" href="https://kapitaalutrecht.nl/"> Kapitaal</a>. They became captivated by the technique
+            of screen-printing and immediately agreed to go printing again, this time on t-shirts. The thrill of creating
+            their own clothing convinced them to spend more time understanding how clothes are made. Since then they have 
+            been collecting designs, learning the basics of sewing and sourcing sustainable t-shirts to print on. All with 
+            the aim of creating cool t-shirts in a manner that is respectful of both humans and the environment through
+            the entire production chain. 
+          </p>
+        </div>
+      </div>
+    </article>
   </Site>
 );
