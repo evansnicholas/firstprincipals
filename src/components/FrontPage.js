ca8007bbae183d254639c17bb842f85534a37294
Nicholas Evans <nick@nicholass-mbp.home>
2019-07-22 22:00:43 +0200

    Adding carousel to front page.

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,14 +1,58 @@
 import React from "react";
 import rethinkingtshirts from "./images/rethinking-tshirts-closer.jpg";
-import Footer from "./Footer";
-import { OutboundLink } from "gatsby-plugin-google-analytics";
+import nowtakingorders from "./images/launch-tshirts.jpg";
+import nextprint from "./images/next_print.jpg"
+import Carousel from 'nuka-carousel';
 
-export default () => (
-  <div className="mw7 center avenir">
-    <article className="db ph3 ph2-l mb4 br2-ns no-underline bg-white black">
-      <div className="w-100">
-        <img src={rethinkingtshirts} class="w-100" alt="rethinking t-shirts" />
+export default class extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.carousel = React.createRef();
+  }
+
+  componentDidMount() {
+    // This is necessary to ensure the carousel resizes to the correct size after first load (otherwise
+    //it sets the size before the image has loaded and then the slide has no height).
+    this.setDimensions();
+  }
+
+  // It is a bit paranoid to call this also onLoad of the image, but we want to be 100% sure the 
+  // carousel sizes correctly.
+  setDimensions() {
+    setTimeout(() => {
+      this.carousel.current.setDimensions();
+    }, 0);
+  }
+
+  render() {
+    return (
+      <div className="mw7 center avenir">
+        <article className="db ph3 ph2-l mb4 br2-ns no-underline bg-white black">
+          <Carousel
+            ref={this.carousel}
+            heightMode="first"
+            autoplay={true}
+            speed={500}
+            wrapAround={true}
+            renderCenterLeftControls={({ previousSlide }) => null }
+            renderCenterRightControls={({ nextSlide }) => null }
+            >
+            <img 
+              src={rethinkingtshirts}
+              className="w-100"
+              alt="rethinking t-shirts" onLoad={() => this.setDimensions()}/>
+            <img 
+              src={nowtakingorders}
+              className="w-100"
+              alt="now taking orders" />
+            <img 
+              src={nextprint}
+              className="w-100"
+              alt="nextprint" />
+          </Carousel>
+        </article>
       </div>
-    </article>
-  </div>
-);
+    )
+  };
+};
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,9 @@ import { OutboundLink } from "gatsby-plugin-google-analytics";
 export default () => (
   <header className="bg-white tc pv4 avenir">
     <Link className="dib w5" to="/">
-      <img src={logo} class="w-100" alt="" />
+      <img src={logo} className="w-100" alt="" />
     </Link>
-    <nav class="tc center bt bb b--black-10 mt4">
+    <nav className="tc center bt bb b--black-10 mt4">
       <OutboundLink
         className="gray fw5 f6 f5-ns ttu link bg-animate hover-black hover-bg-yellow dib pa3 ph4-l"
         href="https://store.fromfirstprincipals.com/"
