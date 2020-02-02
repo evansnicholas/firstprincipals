930e6f4f0bb9284ec9e4183c38b4cfa2b9a2c593
Nicholas Evans <nick@nicholass-mbp.home>
2020-02-02 11:57:25 +0100

    Adding cookie consent and Privacy Policy.

---
+++ src/components/CookieConsent.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { CSSTransition } from "react-transition-group";
+import styles from "./CookieConsent.module.scss";
+import { CookieConsentKey } from "../constants";
+import { Link } from "gatsby";
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      displayCookieConsent: false
+    };
+  }
+
+  handleClick() {
+    this.setCookieConsent();
+    this.setState({
+      displayCookieConsent: false
+    });
+  }
+
+  componentDidMount() {
+    const cookieConsent = this.getCookieConsent();
+    if (cookieConsent) {
+      // The user has already accepted cookies.
+      return;
+    }
+    this.setState({
+      displayCookieConsent: true
+    });
+  }
+
+  getCookieConsent() {
+    if (window && window.localStorage) {
+      const consent = window.localStorage.getItem(CookieConsentKey);
+      return Boolean(consent);
+    }
+  }
+
+  setCookieConsent() {
+    if (window && window.localStorage) {
+      window.localStorage.setItem(CookieConsentKey, true);
+    }
+  }
+
+  render() {
+    const inProp = this.state.displayCookieConsent;
+
+    return (
+      <div>
+        <CSSTransition
+          in={inProp}
+          unmountOnExit
+          classNames={{
+            exit: styles.cookieConsentExit,
+            exitActive: styles.cookieConsentExitActive
+          }}
+          timeout={500}
+        >
+          <div
+            key="cookie-transition"
+            className="flex flex-wrap items-center tl pv3 bb b--black-10"
+          >
+            <div className="w-75-ns w-100 ph0-l ph3 pb0-ns pb2 tl-ns">
+              By continuing to use this website you agree to our <Link className="black" to="/privacy">Privacy Policy</Link>.
+            </div>
+            <div className="ph0-ns ph3">
+              <button
+                onClick={this.handleClick}
+                className="f6 br2 pa2 bn white bg-black b pointer"
+              >
+                I AGREE
+              </button>
+            </div>
+          </div>
+        </CSSTransition>
+      </div>
+    );
+  }
+}
---
+++ src/constants/LocalStorageKeys.js
@@ -0,0 +1,1 @@
+export const CookieConsentKey = 'cookieConsent';
