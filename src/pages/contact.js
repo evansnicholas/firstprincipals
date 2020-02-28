97ce6e22d48aaef7119560acc12c317923a7f39b
Nicholas Evans <nick@nicholass-mbp.home>
2020-02-28 23:45:20 +0100

    Refactoring contact page to stateful react form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
 import Site from "../components/Site";
+import axios from 'axios';
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
+const FormNotSubmittedStatus = "formNotSubmitted";
+const FormSubmitSuccessStatus = "formSubmitSuccess";
+const FormSubmitErrorStatus = "formSubmitError";
 
 
-export default () => (
-    <Site>
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>   
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-    </Site>
-);
+export default () => {
+
+    const [name, setName] = useState("");
+    const [formStatus, setFormStatus] = useState(FormNotSubmittedStatus);
+
+    const handleSubmit = (e) => {
+        const formData = {
+            name
+        }
+        axios.post("/", {
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          data: encode({ "form-name": "contact", ...formData })
+        })
+          .then(() => setFormStatus(FormSubmitSuccessStatus))
+          .catch(error => {console.log(error); setFormStatus(FormSubmitErrorStatus)});
+    
+        e.preventDefault();
+      };
+
+    const renderForm = () => (
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            Your Name:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
+
+    const renderSuccessMessage = () => (
+      <p>Thanks for getting in touch, we will respond as soon as possible.</p>
+    );
+
+    const renderErrorMessage = () => (
+      <p>Something went wrong submitting the form, please try again.</p>
+    );
+
+    const renderContact = () => {
+        switch (formStatus) {
+            case FormNotSubmittedStatus: return renderForm();
+            case FormSubmitSuccessStatus: return renderSuccessMessage();
+            case FormSubmitErrorStatus: return renderErrorMessage();
+            default: return renderErrorMessage();
+        }
+    }
+
+    return (
+      <Site>
+        {renderContact()}
+      </Site>
+    );
+}
+
+    
