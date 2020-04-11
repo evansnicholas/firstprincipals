6d297018e4c147f75e35b23b3973cf6a1fa00eca
Nicholas Evans <nick@nicholass-mbp.home>
2020-04-11 10:29:58 +0200

    Adding timeline image and trust pilot link

---
+++ src/components/Libs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export default () => (
+    <Helmet>
+      <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+    </Helmet>
+)
