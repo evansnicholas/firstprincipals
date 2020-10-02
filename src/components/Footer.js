c4b7a0e90a22d88f8938e4de25457c92c5817a1d
Nicholas Evans <nick@Nicholass-MacBook-Pro.local>
2020-10-02 15:52:23 +0100

    Rectoring menu to fix no scroll jump issue.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -74,7 +74,7 @@ export default class Footer extends React.Component {
               Email Address
             </label>
             <input
-              className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
+              className="f6 f7-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
               placeholder="Your Email Address"
               type="text"
               name="EMAIL"
--- src/components/Site.js
+++ src/components/Site.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SEO from "./SEO";
 import Libs from "./Libs";
+import Content from "./Content";
 import './Site.scss';
 
-export default ({ children }) => (
-  <>
-    <SEO />
-    <Libs />
-    <div className="w-100 mw7 center avenir">
-      <Navbar />
-      {children}
-      <Footer />
+export default ({ children }) => {
+  const [contentFaded, fadeContent] = useState(false);
+
+  return (
+    <div>
+      <SEO />
+      <Libs />
+      <div className="w-100 mw7 center avenir">
+        <Navbar fadeContent={fadeContent} />
+        <Content faded={contentFaded}>{children}</Content>
+        <Footer />
+      </div>
     </div>
-  </>
-);
+  );
+};
